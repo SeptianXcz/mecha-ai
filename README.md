f9e659f795ec3891d0683443cca86d049d222193
Jabalsurya2105 <jabalsurya1846@gmail.com>
2024-05-05 16:24:03 +0700

    mecha-ai update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mecha AI
+
+Mecha AI is a Bot with an AI (artificial intelligence) artificial intelligence program.
+This bot system is able to play an active role as a virtual assistant that replies to every message automatically
+
+This Bot is Designed and Developed By Jabal Surya.
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(PORT, () => {
+console.log(`Server is running on http://localhost:${PORT}`);
+});
