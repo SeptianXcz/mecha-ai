02d96a652a94da60c7f45c868cf30c727a528c81
Jabal Surya Ngalam <87789231+Jabalsurya2105@users.noreply.github.com>
2024-05-05 19:57:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, '/index.html'));
+res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
